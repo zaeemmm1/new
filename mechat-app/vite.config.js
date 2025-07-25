28310ca98000b5b18328b34a86c5c3ca5d0ac4f0
zaeemmm1 <zaeemmian19@gmail.com>
2025-07-25 16:42:19 +0000

    up

--- mechat-app/vite.config.js
+++ mechat-app/vite.config.js
@@ -1,11 +1,13 @@
-import { defineConfig } from 'vite'
-import react from '@vitejs/plugin-react'
+import { defineConfig } from "vite";
+import react from "@vitejs/plugin-react";
 
 // https://vite.dev/config/
 export default defineConfig({
   base: "/",
   plugins: [react()],
-   server: {
-    host: true
-  }
+  server: {
+    allowedHosts: [
+      "a1d328b2-25e3-4ac3-bb52-fbd8bc1bd0d6-00-2xxsyfgxc6ftk.sisko.replit.dev",
+    ],
+  },
 });
